9e8c75907244d4912d08f62da37d08dc2c94a46b
Bogdan Neterebskii <bog2dan1@gmail.com>
2018-10-20 22:41:27 +0200

    service returns images

--- varian/services/service.py
+++ varian/services/service.py
@@ -1,13 +1,21 @@
+import base64
 import os
+import tempfile
+
 from flask import Flask, request, jsonify
 from flask_cors import CORS
 from werkzeug.utils import secure_filename, redirect
 import numpy as np
+import zipfile
+from src import preprocess
+from matplotlib import pyplot as plt
 
 app = Flask(__name__)
 CORS(app)
 
-UPLOADER_FOLDER='/tmp/upload'
+UPLOADER_FOLDER = '/tmp/upload'
+UNZIP_FOLDER = '/tmp/unzip'
+RESULT_FOLDER = '/tmp/results'
 
 EXAMPLE='varian/services/380677.npz'
 
@@ -24,28 +32,67 @@ def ping():
 
 @app.route('/v1/predict', methods=['POST'])
 def predict():
+    ### read
     if 'file' not in request.files:
         print('No file part')
         return redirect(request.url)
 
     file = request.files['file']
-    filename = secure_filename(file.filename)
 
+    filepath = save(file)
+    directory = unzip(filepath)
+
+    # here preprocess and so on
+    #xs = preprocess.process_test(directory)
+
+    l = np.load('varian/services/380677.npz')
+    xs = l['X']
+    xs_new = np.array(xs / xs.max(), dtype=np.float32)
+
+
+    results = make_pictures(filepath, xs_new)
+    print(results)
+    return jsonify(results)
+
+
+def save(file):
+    filename = secure_filename(file.filename)
     if not os.path.exists(UPLOADER_FOLDER):
-        os.mknod(UPLOADER_FOLDER)
+        os.makedirs(UPLOADER_FOLDER)
 
     filepath = os.path.join(UPLOADER_FOLDER, filename)
-
     file.save(filepath)
-    # print(r)
-    # content = request.json
-    print(5)
 
-    # res = predict(content)
-    return jsonify(len([5]))
+    return filepath
 
+def unzip(filepath):
+    zip_ref = zipfile.ZipFile(filepath, 'r')
 
-def predict(req):
-    response = req
-    response['data'] = [{'info': 'abracadabra', 'source': req['data'][0]}]
-    return response
+    directory = os.path.join(UNZIP_FOLDER, '{}-dir'.format(os.path.basename(filepath)))
+
+    zip_ref.extractall(directory)
+    zip_ref.close()
+
+    files = [f for f in os.listdir(directory) if not f.startswith(".")]
+
+    return os.path.join(directory, files[0])
+
+# later xs_old, xs, ys
+def make_pictures(filepath, xs_old):
+    print(xs_old.shape)
+    result_folder = os.path.join(RESULT_FOLDER, '{}-dir'.format(os.path.basename(filepath)))
+    print(result_folder)
+    if not os.path.exists(result_folder):
+        os.makedirs(result_folder)
+
+    results = []
+    for i, xs_item in enumerate(xs_old):
+        filename = os.path.join(result_folder, '{}.png'.format(i))
+
+        plt.imsave(filename, xs_item, cmap=plt.get_cmap('gray'))
+
+        with open(filename, 'rb') as f:
+            encoded = str(base64.b64encode(f.read()))
+            results.append(encoded)
+
+    return results
