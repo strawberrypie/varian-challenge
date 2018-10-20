88e45db9e88229d28b8d23c8c9d56c928ab9dfcb
Inna Yarantseva <innayarantseva@gmail.com>
2018-10-20 19:29:58 +0200

    front cleanup

--- front/src/App.js
+++ front/src/App.js
@@ -6,28 +6,17 @@ import Result from './Result';
 import './App.css';
 
 class App extends Component {
-  render() {
-    return (
-      <div className="App">
-
-        {/*<header className="App-header">
-          <a
-            className="App-link"
-            href="https://reactjs.org"
-            target="_blank"
-            rel="noopener noreferrer"
-          >
-            Learn React
-          </a>
-        </header>*/}
-        <Switch>
-            <Route exact path="/"        component={ Main   } />
-            <Route       path="/predict" component={ Form   } />
-            <Route       path="/result"  component={ Result } />
-        </Switch>
-      </div>
-    );
-  }
+    render() {
+        return (
+            <div className="App">
+                <Switch>
+                    <Route exact path="/"        component={ Main   } />
+                    <Route       path="/predict" component={ Form   } />
+                    <Route       path="/result"  component={ Result } />
+                </Switch>
+            </div>
+        );
+    }
 }
 
 export default App;
